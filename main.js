f1dc78bd8a68a46cd414607629f76fdf975fa220
yogiedigital <yogiedigital@gmail.com>
2024-02-17 21:45:56 +0700

    last add

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const url = require('url');
-const path = require('path');
+// const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -20,12 +20,15 @@ function createWindow() {
   // });
 
   // console.log(path.join(__dirname, 'index.html'))
-  const parentDir = path.resolve(__dirname, '..');
-  const dirReact = path.join(parentDir, 'test-dss-reactjs/dist/index.html');
-  const newDir = path.resolve(dirReact);
+  // const parentDir = path.resolve(__dirname, '..');
+  // const dirReact = path.join(parentDir, 'test-dss-reactjs/dist/index.html');
+  // const newDir = path.resolve(dirReact);
   // console.log(parentDir)
   // console.log(dirReact)
-  console.log(newDir)
+
+  // const yogieDir = path.resolve(__dirname, 'build/index.html');
+  // const yogieDir = `file://${__dirname}/build/index.html`;
+  // console.log(yogieDir)
 
   // const startUrl = url.format({
   //   pathname: path.join(__dirname, 'index.html'),
@@ -37,9 +40,12 @@ function createWindow() {
 
   // console.log("aaa");
 
-  win.loadFile(dirReact);
+  // win.loadFile(yogieDir);
+  // win.loadURL(`file://${__dirname}/build/index.html`);
+
+  // win.loadFile(dirReact);
   // win.loadURL('http://localhost:5173/halo');
-  // win.loadURL('http://localhost:5173');
+  win.loadURL('http://localhost:5173');
 
   // Open DevTools if in development mode
   if (process.env.NODE_ENV === 'development') {
@@ -47,7 +53,8 @@ function createWindow() {
   }
 }
 
-app.whenReady().then(createWindow);
+// app.whenReady().then(createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -59,6 +66,17 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+ipcMain.on('login', (event, { username, password }) => {
+  // Perform authentication logic here
+  console.loog("username", username)
+  console.loog("password", password)
+  // if (username === 'admin' && password === 'password') {
+  //   event.reply('login-success');
+  // } else {
+  //   event.reply('login-failure');
+  // }
 });
 
 // function createMainWindow() {
