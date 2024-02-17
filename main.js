fcd7af5cf3693042baeeeb775a917b874d554c42
yogiedigital <yogiedigital@gmail.com>
2024-02-17 16:16:55 +0700

    set main js config

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
 const { app, BrowserWindow } = require('electron');
-// const url = require('url');
-// const path = require('path');
+const url = require('url');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
+      sandbox: false,
+      contentSecurityPolicy: "default-src 'self'; script-src 'self';",
     },
   });
 
-  win.loadFile('index.html');
+  // const startUrl = url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: file
+  // });
+
+  // console.log(path.join(__dirname, 'index.html'))
+  const parentDir = path.resolve(__dirname, '..');
+  const dirReact = path.join(parentDir, 'test-dss-reactjs/dist/index.html');
+  const newDir = path.resolve(dirReact);
+  // console.log(parentDir)
+  // console.log(dirReact)
+  console.log(newDir)
+
+  // const startUrl = url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: file
+  // });
+  // console.log(startUrl)
+
+  win.webContents.openDevTools();
+
+  // console.log("aaa");
+
+  win.loadFile(dirReact);
+  // win.loadURL('http://localhost:5173/halo');
+  // win.loadURL('http://localhost:5173');
 
   // Open DevTools if in development mode
   if (process.env.NODE_ENV === 'development') {
